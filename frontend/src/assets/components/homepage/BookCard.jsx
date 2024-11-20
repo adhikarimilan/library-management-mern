1a3f1d0c827c5f7818988a5548a320e624aa0b54
adhikarimilan <38108519+adhikarimilan@users.noreply.github.com>
2024-11-20 23:43:43 +1100

    added card display for books option

--- frontend/src/assets/components/homepage/BookCard.jsx
+++ frontend/src/assets/components/homepage/BookCard.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { BsInfoCircle } from "react-icons/bs";
+import { PiBookOpenTextLight } from "react-icons/pi";
+import { AiOutlineEdit } from "react-icons/ai";
+import { MdOutlineDelete } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const BookCard = ({ books }) => {
-  return <div>BookCard</div>;
+  return (
+    <div className="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+      {books.map((book, index) => (
+        <div
+          className="border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl"
+          key={index}
+        >
+          <h2 className="absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg">
+            {book.publishYear}
+          </h2>
+          <div className="justify-start items-center gap-x-2">
+            <PiBookOpenTextLight className="text-blue-300 text-2xl " />
+            <h2 className="my-1">{book.author}</h2>
+          </div>
+          <div className="flex justify-between items-center gap-x-2 mt-4 p-4">
+            <Link to={`/books/show/${book._id}`}>
+              <BsInfoCircle className="text-2xl text-green-800 hover:text-black"></BsInfoCircle>
+            </Link>
+            <Link to={`/books/edit/${book._id}`}>
+              <AiOutlineEdit className="text-2xl text-green-800 hover:text-black"></AiOutlineEdit>
+            </Link>
+            <Link to={`/books/delete/${book._id}`}>
+              <MdOutlineDelete className="text-2xl text-red-800 hover:text-light-red"></MdOutlineDelete>
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default BookCard;
